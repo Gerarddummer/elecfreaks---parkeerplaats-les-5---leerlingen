58cbaef13eb8edf4a3ed8e556fbf6264a3ffb6bb
Gerarddummer <gerarddummer@hotmail.com>
2023-10-25 11:18:10 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    Motor_slagboom_uitgang_open()
+    basic.pause(1000)
+    Motor_slagboom_uitgang_dicht()
+})
+function Motor_Slagboom_ingang_open () {
+    neZha.setMotorSpeed(neZha.MotorList.M2, 25)
+    basic.pause(500)
+    neZha.stopMotor(neZha.MotorList.M2)
+}
+function Motor_slagboom_uitgang_open () {
+    neZha.setMotorSpeed(neZha.MotorList.M1, 25)
+    basic.pause(200)
+    neZha.stopMotor(neZha.MotorList.M1)
+}
+input.onButtonPressed(Button.B, function () {
+    Motor_Slagboom_ingang_open()
+    basic.pause(1000)
+    Motor_Slagboom_ingang_dicht()
+})
+function Motor_Slagboom_ingang_dicht () {
+    neZha.setMotorSpeed(neZha.MotorList.M2, -25)
+    basic.pause(200)
+    neZha.stopMotor(neZha.MotorList.M2)
+}
+function Motor_slagboom_uitgang_dicht () {
+    neZha.setMotorSpeed(neZha.MotorList.M1, -25)
+    basic.pause(200)
+    neZha.stopMotor(neZha.MotorList.M1)
+}
